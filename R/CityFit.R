277e618e5e4680ea492c940c41bcfc8da1f47e55
my1120 <meilin.yan@colostate.edu>
2017-01-24 13:01:30 -0700

    add more functions

---
+++ R/CityFit.R
@@ -0,0 +1,54 @@
+#' Fit distributed lag model for one city.
+#'
+#' This function uses dataframe created by \code{CrossoverData} and fits a
+#' distributed lag model for this city.
+#'
+#' @inheritParams readCity
+#' @inheritParams readStorm
+#' @inheritParams CrossoverData
+#'
+#' @param cause A character vector giving the death outcome used in the model.
+#' @param lag_knots A numeric vector giving the knots for \code{lag} in the
+#' \code{crossbasis()} function.
+#'
+#' @return Prints the abbreviation of city's name and returns a list of
+#' \code{glm()} model object and predicted results by \code{crosspred()}
+#' function from \code{dlnm} package.
+#'
+#' @examples
+#' \dontrun{
+#' fit_detroit <- CityFit(root = "~/tmp/NMMAPS/", criterion = "rain50",
+#'                        city = "det", cause = "accident")
+#' }
+#'
+#' @export
+CityFit <- function(root = c(), criterion = c(), city = c(), cause = "all",
+                    control_ratio = 15, lags = 14, lag_knots = logknots(14, 4),
+                    storm_id = NA){
+  print(city)
+
+  df <- CrossoverData(root, criterion, city,
+                      control_ratio, lags, storm_id)
+
+  cb <- dlnm::crossbasis(df$hurr, lag = c(0, lags),
+                         argvar = list(fun = "lin"),
+                         arglag = list(fun = "ns", knots = lag_knots))
+
+  # if this city has only one storm, "stratum" will not be included in the model.
+  if(nrow(subset(df, hurr == 1)) == 1){
+    city_fit <- glm(df[, cause] ~ cb + splines::ns(year, 2),
+                    family = quasipoisson(link = log),
+                    data = df,
+                    control = glm.control(epsilon = 10E-8, maxit = 5000))
+  }else{
+    city_fit <- glm(df[, cause] ~ cb + splines::ns(year, 2) + stratum,
+                    family = quasipoisson(link = log),
+                    data = df,
+                    control = glm.control(epsilon = 10E-8, maxit = 5000))
+  }
+
+  city_pred <- dlnm::crosspred(basis = cb, model = city_fit, at = 1)
+
+  fit_city <- list(city_fit = city_fit, city_pred = city_pred)
+  return(fit_city)
+}
---
+++ R/MetaReg.R
@@ -0,0 +1,106 @@
+#' Pull Coefficients and Variance-Covariance matrix.
+#'
+#' This function pulls coefficients and variance-covariance matrix from \code{crosspred()}
+#' object.
+#'
+#' @param CityFit A fitted model object.
+#'
+#' @return A matrix of coefficients/variance-covariance between the parameters
+#' estimates in the model.
+#'
+#' @export
+pull_city_coef <- function(CityFit){
+  city_coef <- coef(CityFit[["city_pred"]])
+  return(city_coef)
+}
+
+pull_city_vcov <- function(CityFit){
+  city_vcov <- vcov(CityFit[["city_pred"]])
+  return(city_vcov)
+}
+
+
+#' Univariate Meta-regression.
+#'
+#' This function performs a univariate meta-regression using \code{mvmeta()}
+#' function in the \code{mvmeta} package.
+#'
+#' @inheritParams readStorm
+#' @inheritParams CityFit
+#' @inheritParams mvmeta::mvmeta
+#'
+#' @param city_list A list of abbreviation of cities' names
+#'
+#' @return Returns a list of class \code{'mvmeta'}.
+#'
+#' @references
+#'
+#' Gasparrini A., Armstrong, B., Kenward M. G. (2012). Multivariate
+#' meta-analysis for non-linear and other multi-parameter associations.
+#' Statistics in Medicine. 31(29):3821-3839.
+#'
+#' @examples
+#' \dontrun{
+#' city_list <- c("no", "lkch", "miam", "jckv", "mobi")
+#' meta <- UniMetaReg(city_list = city_list, criterion = "rain75",
+#'                    cause = "accident")
+#' }
+#'
+#' @export
+UniMetaReg <- function(city_list = c(), criterion = c(), cause = "all",
+                       method = "reml"){
+
+  city_fit <- lapply(city_list, CityFit, criterion = criterion, cause = cause)
+  city_coefs <- lapply(city_fit, pull_city_coef)
+  city_coefs <- do.call("rbind", city_coefs)
+
+  city_vcovs <- lapply(city_fit, pull_city_vcov)
+
+  meta_fit <- mvmeta::mvmeta(city_coefs ~ 1, S = city_vcovs, method = method)
+
+  return(meta_fit)
+}
+
+
+#' Prediction for a meta-regression
+#'
+#' This function uses \code{crosspred()} function in the \code{dlnm} package to
+#' predict the
+#'
+#' @param meta_model A model object of class \code{'mvmeta'}.
+#' @param exposure A numeric vector giving the exposure value for prediction.
+#'
+#' @return Returns a list of class \code{'crosspred'}.
+#'
+#' @examples
+#' \dontrun{
+#' exposure <- c(rep(0, 20), 1, rep(0, 20))
+#' pred <- pred_meta(meta_model = meta, exposure = exposure)
+#' }
+#'
+#' @export
+pred_meta <- function(meta_model, exposure = c()){
+  hurr_basis <- dlnm::crossbasis(x = exposure,
+                                 lag = c(0, 14),
+                                 argvar = list(fun = "lin"),
+                                 arglag = list(fun = "ns",
+                                               knots = dlnm::logknots(14, 4)))
+
+  meta_pred <- crosspred(hurr_basis, coef = coef(meta_model),
+                         vcov = vcov(meta_model), model.link = "log",
+                         cen = 0, at = 1)
+  return(meta_pred)
+}
+
+
+#' Plot the prediction of a meta-regression.
+#'
+#' @param meta_pred A \code{'crosspred'} class object.
+#' @param title A character string giving the title of the plot.
+#'
+#' @export
+plot_meta <- function(meta_pred, title = c()){
+  plot(meta_pred, xlab = "Lag",
+       ylab = "RR compared to non-storm day",
+       exp = TRUE, ptype = "slices", var = 1, main = title)
+}
